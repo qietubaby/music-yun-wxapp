c7a879172749cafe747de8d9c8a72ba1ac56defc
qietubaby <201951244@qq.com>
2020-03-13 18:10:47 +0800

    add musiclist page

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -1,15 +1,20 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
+
 const TcbRouter = require('tcb-router')
+
+const rp = require('request-promise')
+
+const BASE_URL = 'http://musicapi.xiecheng.live'
 
 cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const app = new TcbRouter({event})
+  const app = new TcbRouter({ event })
 
   // 路由的方式返回数据
-  app.router('playlist', async(ctx,next) => {
+  app.router('playlist', async (ctx, next) => {
     ctx.body = await cloud.database().collection('playlist')
       .skip(event.start)
       .limit(event.count)
@@ -19,6 +24,22 @@ exports.main = async (event, context) => {
         return res
       })
   })
+
+  app.router('musiclist', async (ctx, next) => {
+    ctx.body = await rp(BASE_URL + '/playlist/detail?id=' + parseInt(event.playlistId))
+      .then((res) => {
+        return JSON.parse(res)
+      })
+    // ctx.body = await cloud.database().collection('playlist')
+    //   .skip(event.start)
+    //   .limit(event.count)
+    //   .orderBy('createTime', 'desc')
+    //   .get()
+    //   .then((res) => {
+    //     return res
+    //   })
+  })
+
   return app.serve()
 
 
---
+++ miniprogram/components/musiclist/musiclist.js
@@ -0,0 +1,28 @@
+// components/musiclist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    musiclist: Array
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    playingId: -1
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onSelect(e){
+      console.log(e)
+      this.setData({
+        playingId: e.currentTarget.dataset.musicid
+      })
+    }
+  }
+})
